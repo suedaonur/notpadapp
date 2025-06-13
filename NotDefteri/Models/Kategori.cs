e6f0c6f266febff9590e5f1d957b765964001feb
Sueda Onur <suedaonur@gmail.com>
2025-06-13 08:34:48 +0300

    Son degisiklikler güncellendi

---
+++ NotDefteri/Models/Kategori.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotDefteri.Models
+{
+    public class Kategori
+    {
+        public string CategoryName { get; set; }
+        public List<Not> Notlar { get; set; }
+        public Kategori() 
+        {
+            Notlar = new List<Not>();
+        }
+    }
+}
---
+++ NotDefteri/Models/Not.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotDefteri.Models
+{
+    public class Not
+    {
+        public string Header { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+
+        public Not()
+        {
+            CreatedAt = DateTime.Now;
+            LastUpdatedAt = DateTime.Now;
+        }
+    }
+}
